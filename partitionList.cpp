941b479938482228c8b04839a98f2b9cd54e1bc6
Heiko Koehler <heiko@pernixdata.com>
2016-08-06 22:58:32 -0700

    fixed partitionList

--- partitionList.cpp
+++ partitionList.cpp
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/partition-list/
+
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -8,35 +10,26 @@
  */
 class Solution {
 public:
-    ListNode* partition(ListNode* head, int x) {
-        ListNode *lesser = nullptr;
-        ListNode *lesserRear = nullptr;
-        ListNode *greater = nullptr;
-        ListNode *greaterRear = nullptr;
+    ListNode *listAppend(ListNode *rear, ListNode *item) {
+        rear->next = item;
+        return item;
+    }
 
+    ListNode* partition(ListNode* head, int x) {
+        ListNode lesserHead(0);
+        ListNode greaterHead(0);
+        ListNode *lesserRear = &lesserHead;
+        ListNode *greaterRear = &greaterHead;
+        
+        if (!head)
+            return NULL;
         for (ListNode *curr = head; curr; curr = curr->next)
-            if (curr->val < x) {
-                if (lesser) {
-                    lesserRear->next = curr;
-                    lesserRear = curr;
-                } else {
-                    lesser = curr;
-                    lesserRear = curr;
-                }
-            } else {
-                if (greater) {
-                    greaterRear->next = curr;
-                    greaterRear = curr;
-                } else {
-                    greater = curr;
-                    greaterRear = curr;
-                }
-            }
-        if (lesserRear)
-            lesserRear->next = greater;
-        else
-            return greater;
-        return lesser;
+            if (curr->val < x)
+                lesserRear = listAppend(lesserRear, curr);
+            else
+                greaterRear = listAppend(greaterRear, curr);
+        greaterRear->next = NULL;
+        lesserRear->next = greaterHead.next;
+        return lesserHead.next;
     }
 };
-
